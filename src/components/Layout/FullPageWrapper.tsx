ad07612dcb61fabba1f748857a716088490fa8b5
Jon <jcadeichmann@gmail.com>
2025-06-08 18:33:49 -0500

    Fix scroll issue for chrome

--- src/components/Layout/FullPageWrapper.tsx
+++ src/components/Layout/FullPageWrapper.tsx
@@ -1,6 +1,6 @@
 export default function FullPageWrapper({ children }: { children: React.ReactNode }) {
     return (
-        <main className="h-screen overflow-y-auto px-[var(--spacing-sm)] py-[var(--spacing-lg)] pb-[89px] md:px-[40px] md:pt-[32px] md:pb-[123px] lg:py-[32px] w-full">
+        <main className="min-h-dvh overflow-y-auto px-[var(--spacing-sm)] py-[var(--spacing-lg)] pb-[89px] md:px-[40px] md:pt-[32px] md:pb-[123px] lg:py-[32px] w-full">
             {children}
         </main>
     );
